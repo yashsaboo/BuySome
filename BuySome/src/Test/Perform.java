3acee93b95f10f5d550c54dd263aed9f2db6c6ab
Yash_Saboo <Yash_Saboo@W282048030P8Q.SYMC.SYMANTEC.COM>
2018-02-11 17:04:46 +0530

    First Commit

---
+++ BuySome/src/Test/Perform.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Perform {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
---
+++ BuySome/src/cv3/Base3.java
@@ -0,0 +1,42 @@
+package cv3;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Base3 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Base3 window = new Base3();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Base3() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1001, 736);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
